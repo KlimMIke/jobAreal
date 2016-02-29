c55b4bdb13a32a9ccce562efe45e2ee49c4b0716
klimychevmn <plagonit@gmail.com>
2016-02-29 17:33:12 +0300

    css + validation js

--- Form(ver1)/validationforms.js
+++ Form(ver1)/validationforms.js
@@ -2,29 +2,47 @@ function validate_form(){
     var form = document.form;
 
     if(!form.lastname.value.match(/\S\D/i)) {
-        alert("Введите пожалуйста свою фамилию!");
+        document.getElementById("lastname_err").className = "errorValid show";
+        document.getElementById("lastname_err").innerHTML = "Введите пожалуйста свою фамилию!";
         return false;
+    }
+    if(form.lastname.value.match(/\S\D/i)) {
+        document.getElementById("lastname_err").className = "errorValid";
     }
 
     if(!form.firstname.value.match(/\S\D/i)) {
-        alert("Введите пожалуйста свое имя!");
+        document.getElementById("firstname_err").className = "errorValid show";
+        document.getElementById("firstname_err").innerHTML = "Введите пожалуйста своё имя!";
         return false;
+    }
+    if(form.firstname.value.match(/\S\D/i)) {
+        document.getElementById("firstname_err").className = "errorValid";
     }
 
     if(!form.sex.value) {
-        alert("Выберете пожалуйста свой пол!");
+        document.getElementById("sex_err").className = "errorValid show";
+        document.getElementById("sex_err").innerHTML = "Введите пожалуйста своё пол!";
         return false;
+    }
+    if(form.sex.value) {
+        document.getElementById("sex_err").className = "errorValid";
     }
 
     if(!form.email.value.match(/\S\w+@\S\w+\.[a-z]{2,5}/i)) {
-        alert("Введите пожалуйста свой e-mail");
+        document.getElementById("email_err").className = "errorValid show";
+        document.getElementById("email_err").innerHTML = "Введите пожалуйста свой email!";
         return false;
+    }
+    if(form.email.value.match(/\S\w+@\S\w+\.[a-z]{2,5}/i)) {
+        document.getElementById("email_err").className = "errorValid";
     }
 
     if(!form.phone.value.match(/\d/i)) {
-        alert("Введите пожалуйста свой телефон!");
+        document.getElementById("phone_err").className = "errorValid show";
+        document.getElementById("phone_err").innerHTML = "Введите пожалуйста свой контактный телефон!";
         return false;
     }
-
-
+    if(form.phone.value.match(/\d/i)) {
+        document.getElementById("phone_err").className = "errorValid";
+    }
 }
